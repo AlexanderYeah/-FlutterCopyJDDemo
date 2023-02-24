c213e31c1c4072c1ad619a941de35691b37ba56f
Alexander <1023954998@qq.com>
2023-02-24 10:30:35 +0800

    flutter_screenutil 插件的封装

--- copy_jd_demo/lib/pages/tabs/home.dart
+++ copy_jd_demo/lib/pages/tabs/home.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
-// 适配的库
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../services/screenAdaper.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -23,10 +21,10 @@ class _HomePageState extends State<HomePage> {
 
   Widget _titleWidget(String title) {
     return Container(
-      height: ScreenUtil().setHeight(30),
-      width: ScreenUtil().setWidth(50),
-      margin: EdgeInsets.only(left: ScreenUtil().setWidth(20)),
-      padding: EdgeInsets.only(left: ScreenUtil().setWidth(20)),
+      height: ScreenAdapter.height(30),
+      width: ScreenAdapter.width(50),
+      margin: EdgeInsets.only(left: ScreenAdapter.width(20)),
+      padding: EdgeInsets.only(left: ScreenAdapter.width(20)),
       decoration: BoxDecoration(
           border: Border(
               left: BorderSide(
@@ -66,15 +64,15 @@ class _HomePageState extends State<HomePage> {
   @override
   // 首先能够上下滑动,则最外层需要包裹一个ListView
   Widget build(BuildContext context) {
-    ScreenUtil.init(context, designSize: const Size(750, 1334));
+    ScreenAdapter.init(context);
     return ListView(
       children: [
         _swiperWidget(),
         SizedBox(
-          height: 15,
+          height: ScreenAdapter.height(15),
         ),
         _titleWidget("You May Like"),
-        SizedBox(height: 15),
+        SizedBox(height: ScreenAdapter.height(15)),
         _titleWidget("You May Like"),
       ],
     );
