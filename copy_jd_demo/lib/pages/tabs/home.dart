f42630251f3632e662da656cd1a55d3072de9f74
Alexander <1023954998@qq.com>
2023-02-24 10:12:01 +0800

    flutter_screenutil 插件适配屏幕

--- copy_jd_demo/lib/pages/tabs/home.dart
+++ copy_jd_demo/lib/pages/tabs/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+// 适配的库
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,6 +18,29 @@ class _HomePageState extends State<HomePage> {
     {"path": "images/HomeBanner/banner_3.jpeg"},
     {"path": "images/HomeBanner/banner_4.jpeg"},
   ];
+
+  //
+
+  Widget _titleWidget(String title) {
+    return Container(
+      height: ScreenUtil().setHeight(30),
+      width: ScreenUtil().setWidth(50),
+      margin: EdgeInsets.only(left: ScreenUtil().setWidth(20)),
+      padding: EdgeInsets.only(left: ScreenUtil().setWidth(20)),
+      decoration: BoxDecoration(
+          border: Border(
+              left: BorderSide(
+        color: Colors.red,
+        width: 6,
+      ))),
+      child: Text(title,
+          style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black54)),
+    );
+  }
+
   // 轮播图
   Widget _swiperWidget() {
     return Container(
@@ -41,9 +66,16 @@ class _HomePageState extends State<HomePage> {
   @override
   // 首先能够上下滑动,则最外层需要包裹一个ListView
   Widget build(BuildContext context) {
+    ScreenUtil.init(context, designSize: const Size(750, 1334));
     return ListView(
       children: [
         _swiperWidget(),
+        SizedBox(
+          height: 15,
+        ),
+        _titleWidget("You May Like"),
+        SizedBox(height: 15),
+        _titleWidget("You May Like"),
       ],
     );
   }
