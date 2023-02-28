ea24c05ebecee9582a669e02724b10e0ea8a1868
Alexander <1023954998@qq.com>
2023-03-01 06:41:19 +0800

    商品详情底部加入购物车和立即购买浮动栏搭建，自定义按钮组件的抽取

---
+++ copy_jd_demo/lib/widget/jdProductButton.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../services/screenAdaper.dart';
+
+typedef JDProductButtonCallback = void Function();
+
+class JDProductButton extends StatelessWidget {
+  // 外部传递过来颜色 和 标题
+  Color color;
+  String text;
+  JDProductButtonCallback? callback;
+
+  JDProductButton(
+      {super.key, this.color = Colors.black, this.text = "按钮", this.callback});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: callback!,
+        child: Container(
+          height: ScreenAdapter.height(70),
+          margin: EdgeInsets.fromLTRB(5, 10, 20, 5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: this.color,
+          ),
+          child: Center(
+              child: Text(this.text, style: TextStyle(color: Colors.white))),
+        ));
+  }
+}
